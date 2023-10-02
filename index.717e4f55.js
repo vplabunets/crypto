23ca3da5dbde0f316ceb5063b92966ecc2b7820c
vplabunets <91912993+vplabunets@users.noreply.github.com>
2023-10-02 08:53:55 +0000

    Deploying to gh-pages from @ vplabunets/crypto@30316490fcedc0157ed8041d2003ad2ad6a7ba36 🚀

---
+++ index.717e4f55.js
@@ -0,0 +1,3 @@
+(()=>{let e=document.querySelector(".agenta-scroll-button"),l=document.querySelectorAll(".agenta-card-list li"),t=window.innerWidth,n=t>744?4:1,o=t>744?4:1;e.addEventListener("click",()=>{l.forEach(e=>{e.style.display="none"});for(let e=n;e<n+o;e++)l[e]&&(l[e].style.display="flex");(n+=o)>=l.length&&(n=0)})})();//# sourceMappingURL=index.717e4f55.js.map
+
+//# sourceMappingURL=index.717e4f55.js.map
